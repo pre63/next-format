869b0225f7965c0d8e159f4292a75ea2d5cf6b84
Simon Deshaies <simon@atomable.io>
2017-03-12 17:01:03 -0400

    added cli

---
+++ bin/index.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+
+const next = require('../')
+const fs = require('fs')
+const path = require('path');
+
+const walker = dir => {
+  const many = [];
+  const walkSync = d => {
+    if (/node_modules/g.test(d)) {
+      return;
+    }
+    const files = fs.readdirSync(d);
+    files.forEach(file => {
+      const p = `${d}/${file}`;
+      if (fs.statSync(p).isDirectory()) {
+        walkSync(p);
+      } else {
+        many.push(path.relative(dir, p));
+      }
+    });
+  };
+  walkSync(dir);
+  return many;
+};
+
+const read = file => fs.readFileSync(file, 'UTF-8');
+const write = file => content => fs.writeFileSync(file, content, 'UTF-8', { flags: 'w+' });
+
+const format = file => write(file)(next(read(file)));
+
+walker(process.cwd()).filter(f => f.toLowerCase().endsWith('.js')).map(format);
--- index.js
+++ index.js
@@ -1,36 +1,40 @@
 const prettier = require('prettier')
 
-const prettify = (source) =>
+const prettify = source =>
   prettier.format(source, {
     printWidth: 100,
     tabWidth: 2,
-    singleQuote: false,
-    trailingComma: "es5",
+    singleQuote: true,
+    trailingComma: 'es5',
     bracketSpacing: true,
     jsxBracketSameLine: true,
-    parser: 'babylon'
+    parser: 'babylon',
   })
 
-const alwaysAString = (s = '') => s
+const alwaysAString = (s = '') => s;
 
-const compose = (...args) => a => args.reduceRight((acc, func) => func(acc), a)
+const compose = (...args) => a => args.reduceRight((acc, func) => func(acc), a);
 
-const replace = (exp, repl) => s => s.replace(exp, repl)
+const replace = (exp, repl) => s => s.replace(exp, repl);
 
-const removeSemi = replace(';', '')
+const removeSemi = replace(';', '');
 
-const removeEolSemi = replace(/;([\n\r])/, '$1')
+const removeEolSemi = replace(/;([\n\r])/, '$1');
 
-const insertImportantSemi = replace(/([^=]\n^[ 	]*)([\(\[`])/gmi, '$1;$2')
+const insertImportantSemi = replace(/([^=>]\n^[ 	]*)([\(\[`])/gmi, '$1;$2');
 
-const endParenthesisRegex = /\)[\n\r 	]+\)/
+const endParenthesisRegex = /\)[\n\r 	]+\)/gmi;
 
-const formatEndParenthesis = source => !endParenthesisRegex.exec(source) ? source : formatEndParenthesis(source.replace(endParenthesisRegex, '))'))
+const formatEndParenthesis = source =>
+  !endParenthesisRegex.exec(source)
+    ? source
+    : formatEndParenthesis(source.replace(endParenthesisRegex, '))'));
 
-export default compose(
+module.exports = compose(
   removeEolSemi,
   insertImportantSemi,
   removeSemi,
   formatEndParenthesis,
   alwaysAString,
-  prettify)
+  prettify
+);
