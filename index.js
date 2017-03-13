4b72812239161d643a2ce82d9d1c2ee230cb987c
Simon Deshaies <simon@atomable.io>
2017-03-12 20:29:10 -0400

    reduced line length to 80, broden the scope of important semi to include tabs and spaces

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const prettier = require('prettier')
 
 const prettify = source =>
   prettier.format(source, {
-    printWidth: 100,
+    printWidth: 80,
     tabWidth: 2,
     singleQuote: true,
     trailingComma: 'es5',
