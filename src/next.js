6f37ea5dfa1851136c0d2656ae9235e5e07db2b1
Simon Deshaies <simon@atomable.io>
2017-04-13 21:28:21 -0400

    v 9

--- src/next.js
+++ src/next.js
@@ -18,21 +18,6 @@ const compose = (...args) => a => args.reduceRight((acc, func) => func(acc), a)
 
 const replace = ([exp, repl]) => s => s.replace(exp, repl)
 
-const removeSemi = replace([/[ 	;]+([\n\r])/gmi, '$1'])
-
-const moveImportantSemi = replace([/(;)([\n\r 	]*)([\[`\(])/gmi, '$2$1$3'])
-
-const insertCurlySpaces = replace([
-  /(={)([^ }]*)(})|(^[^`'/\n]*[^$\\u=]{1,2}{)([^}\n]*[^}])(}[^`\n]*\n)/gmi,
-  '$1$4 $2$5 $3$6',
-])
-
-const leftRegex = /([({[])[\n\r 	]+([{(])/gmi
-const replaceLeft = replace([leftRegex, '$1$2'])
-
-const formatLeft = source =>
-  !leftRegex.exec(source) ? source : formatLeft(replaceLeft(source))
-
 const rightRegex = /([\)\}])[\n\r 	]+([)\]])/gmi
 const replaceRight = replace([rightRegex, '$1$2'])
 
@@ -40,11 +25,7 @@ const formatRight = source =>
   !rightRegex.exec(source) ? source : formatRight(replaceRight(source))
 
 module.exports = compose(
-  // insertCurlySpaces,
-  // formatLeft,
-   formatRight,
-  // removeSemi,
-  // moveImportantSemi,
-  // alwaysAString,
+  formatRight,
+  alwaysAString,
   prettify)
 
