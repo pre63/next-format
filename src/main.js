926edbe2df0b974ef403e3f995ea711076452d83
Guillaume Martin <guillaume.martin555@gmail.com>
2017-05-08 20:04:26 -0400

    Skip files that were not modified since the last run. (#1)
    * Only write to disk if file has changed.
    
    * Save the last run time in a file to skip unmodifed files.

--- src/main.js
+++ src/main.js
@@ -2,9 +2,10 @@ const compose = require('oncha/compose').default
 const next = require('./next')
 const fs = require('fs')
 const path = require('path')
+const crypto = require('crypto')
 
 // IMPURE
-const walker = dir => {
+const walker = lastRunTime => dir => {
   const many = []
   const walkSync = d => {
     if (/node_modules/g.test(d)) {
@@ -13,9 +14,12 @@ const walker = dir => {
     const files = fs.readdirSync(d)
     files.forEach(file => {
       const p = `${d}/${file}`
-      if (fs.statSync(p).isDirectory()) {
+      const stat = fs.statSync(p)
+      const now = new Date().getTime()
+
+      if (stat.isDirectory()) {
         walkSync(p)
-      } else {
+      } else if (stat.mtime.getTime() > lastRunTime || stat.ctime.getTime() > lastRunTime) {
         many.push(path.relative(process.cwd(), p))
       }
     })
@@ -26,21 +30,38 @@ const walker = dir => {
 
 const read = file => fs.readFileSync(file, 'UTF-8')
 
-const write = file => content => fs.writeFileSync(file, content, 'UTF-8', { flags: 'w+' })
+const write = file => content =>
+  fs.writeFileSync(file, content, 'UTF-8', { flags: 'w+' })
 
-const format = file => write(file)(next(read(file)))
+const format = file => {
+  const init = read(file)
+  const res = next(init)
 
-const plurial = count => (count > 1 ? 's' : '')
+  init != res && write(file)(res)
+}
+
+const plural = count => (count === 1 ? '' : 's')
+
+const setLastRunTime = path => write(path)(Date.now())
+
+const getLastRunTime = path => fs.existsSync(path) ? read(path) : 0
 
 // PROGRAM
 const selectPathArg = () => (process.argv[2] || '').concat('/')
 
-const selectPath = () => path.normalize(path.join(process.cwd(), selectPathArg()))
+const selectPath = () =>
+  path.normalize(path.join(process.cwd(), selectPathArg()))
 
 const startDate = Date.now()
 
-const count = walker(selectPath()).filter(f => f.toLowerCase().endsWith('.js')).map(format).length
+const lastRunPath = path.resolve(__dirname, '../.last_' + crypto.createHash('md5').update(selectPath()).digest('hex'))
+
+const count = walker(getLastRunTime(lastRunPath))(selectPath())
+  .filter(f => f.toLowerCase().endsWith('.js'))
+  .map(format).length
+
+setLastRunTime(lastRunPath)
 
 const seconds = (Date.now() - startDate) / 1000
 
-console.log(`Formatted ${count} file${plurial(count)} in ${seconds}s.`)
+console.log(`Formatted ${count} file${plural(count)} in ${seconds}s.`)
